24d7622fdc7614d28ec565d91733e10727b90ff6
Brandon Kindred <brandon.kindred@gmail.com>
2024-04-25 17:48:49 -0400

    Removed unnecessary comments

--- src/main/java/com/looksee/auditManager/AuditController.java
+++ src/main/java/com/looksee/auditManager/AuditController.java
@@ -1,22 +1,5 @@
 package com.looksee.auditManager;
 
-/*
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START cloudrun_pubsub_handler]
-// [START run_pubsub_handler]
 import java.util.Base64;
 import java.util.HashSet;
 import java.util.Optional;
@@ -77,6 +60,7 @@ public class AuditController {
 	 * 
 	 * @param body
 	 * @return
+	 * 
 	 * @throws JsonMappingException
 	 * @throws JsonProcessingException
 	 * @throws ExecutionException
